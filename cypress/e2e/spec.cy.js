68feac1f92eb8e1000bc5f92d7de321bdeafa34c
Vinicius Silva Bagni <vinicius.bagni@hotmail.com>
2025-06-09 09:08:37 -0300

    config eslint to files cypress

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('type').click()
+    cy.url().should('include', '/commands/actions')
+    cy.get('.action-email')
+  })
+})
